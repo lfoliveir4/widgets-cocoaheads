f6b7e18ed4b03d847fb12390ea7bb4ddb57454cc
luis <luis.olivera@olxbr.com>
2024-04-20 17:25:42 -0300

    Initial Commit

---
+++ WidgetsCocoaHeads/WidgetsCocoaHeadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsCocoaHeadsApp.swift
+//  WidgetsCocoaHeads
+//
+//  Created by Luis Filipe Alves de Oliveira on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsCocoaHeadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
